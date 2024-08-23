6b763cbb2ade1dbc3042e96225a2ba6138cec95f
a.bordiian <a.bordiian@godeltech.com>
2024-08-23 18:11:15 +0200

    task 5
    completed task functionality

--- src/store/index.js
+++ src/store/index.js
@@ -8,12 +8,21 @@ export default createStore({
   state: state,
   mutations: {
     addTodo(state, title) {
-      state.todos.push({ id: Date.now(), title });
+      state.todos.push({ id: Date.now(), title, completed: false });
+    },
+    toggleComplete(state, id) {
+      const todo = state.todos.find((todo) => todo.id === id);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
     }
   },
   actions: {
     addTodo({ commit }, title) {
       commit('addTodo', title);
+    },
+    toggleComplete({ commit }, id) {
+      commit('toggleComplete', id);
     }
   },
   getters: {
